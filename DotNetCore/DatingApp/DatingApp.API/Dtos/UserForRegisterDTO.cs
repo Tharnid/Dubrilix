0c6a86d8be5abc98f05c0ed8187e73a175cb261b
Tharnid <jeffmsmithdev@gmail.com>
2019-03-17 16:27:51 -0400

    successfully registed user

---
+++ DotNetCore/DatingApp/DatingApp.API/Dtos/UserForRegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDTO // Data Transfer Object - carries Data between objects
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
