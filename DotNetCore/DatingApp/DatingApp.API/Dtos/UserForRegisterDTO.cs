e5ef35efe043b31ff737071119f85b810a89396f
Tharnid <jeffmsmithdev@gmail.com>
2019-03-19 21:56:14 -0400

    done with no blank users

--- DotNetCore/DatingApp/DatingApp.API/Dtos/UserForRegisterDTO.cs
+++ DotNetCore/DatingApp/DatingApp.API/Dtos/UserForRegisterDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDTO // Data Transfer Object - carries Data between objects
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength= 5, ErrorMessage="You must specify password between 5 and 8 characters")]
         public string Password { get; set; }
     }
 }
