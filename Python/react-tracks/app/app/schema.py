7a40acfaf8bf65d16b18be4a7673288fde2ea66d
Tharnid <jeffmsmithdev@gmail.com>
2019-05-30 14:33:13 -0400

    created a track pretty exciting stuff

--- Python/react-tracks/app/app/schema.py
+++ Python/react-tracks/app/app/schema.py
@@ -4,6 +4,9 @@ import tracks.schema
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
