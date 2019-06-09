051bece7392c3a1d66b23f7a32362e728faee99e
Tharnid <jeffmsmithdev@gmail.com>
2019-06-09 12:17:03 -0400

    oops cross code

--- Python/react-tracks/app/tracks/models.py
+++ Python/react-tracks/app/tracks/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
 class Track(models.Model):
     # id added automatically
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
+    # cascade delete...user deleted....track deleted too
 
 
